dd5c89f172f2694ba99257c1ff72318567af0f1d
russellderoeper <deror007@gmail.com>
2024-09-08 17:26:03 +0100

    objuscated links.

--- scripts.js
+++ scripts.js
@@ -19,16 +19,36 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // Collapsible Portfolio interaction code
 var coll = document.getElementsByClassName("collapsible");
-    var j;
-    
-    for (j = 0; j < coll.length; j++) {
-    coll[j].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-        content.style.display = "none";
-        } else {
-        content.style.display = "block";
-        }
-    });
+var j;
+
+for (j = 0; j < coll.length; j++) {
+coll[j].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+    content.style.display = "none";
+    } else {
+    content.style.display = "block";
     }
+});
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    var email = "russell.roeper" + "@" + "gmail.com";
+    document.getElementById("email").textContent = email;
+
+
+
+    var linkedinUrl = "https://www.linkedin.com/in/russellderoeper/";
+    var githubUrl = "https://github.com/deror007";
+
+    document.getElementById("linkedin").innerHTML = 
+        '<a href="' + linkedinUrl + '" target="_blank">' +
+        '<i class="fa fa-linkedin fa-fw w3-text-white w3-xxlarge w3-margin-right w3-hover-opacity"></i> ' + linkedinUrl +
+        '</a>';
+
+    document.getElementById("github").innerHTML = 
+        '<a href="' + githubUrl + '" target="_blank">' +
+        '<i class="fa fa-github fa-fw w3-text-white w3-xxlarge w3-margin-right w3-hover-opacity"></i> ' + githubUrl +
+        '</a>';
+    });
